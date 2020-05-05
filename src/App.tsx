597f1157701b06437856e352026bbc4c146a2127
tupobo <1028461931@qq.com>
2020-05-05 20:26:39 +0800

    新增简易Button组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-
+import Button, { ButtonSize, ButtonType } from './components/Button/button';
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
-        <h2>Hello World</h2>
-        <h3>Hello World</h3>
-        <br />
-        <code>const a = 1</code>
+        <Button disabled>Hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>
+          Hello
+        </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com">
+          Baidu Link
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
